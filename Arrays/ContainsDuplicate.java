5e1550186f087f9be572483d776fff8dde01cf57
VikashTiwari23 <vikash.tiwari.236968@gmail.com>
2025-11-19 15:52:16 +0530

    ContainsDuplicate

---
+++ Arrays/ContainsDuplicate.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+
+class ContainsDuplicate{
+
+    public static  boolean duplicate(int[] nums){
+        HashSet<Integer> set = new HashSet<>();
+        for(int num: nums){
+            if(set.contains(num)){
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        boolean isDuplicate = duplicate(nums);
+        System.out.println(isDuplicate);
+    }
+
+
+}
