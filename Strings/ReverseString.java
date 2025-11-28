cd6091560597cb519e837a6f995e5d8286d1ce98
VikashTiwari23 <vikash.tiwari.236968@gmail.com>
2025-11-28 22:55:28 +0530

    ReverseString Reupdated

--- Strings/ReverseString.java
+++ Strings/ReverseString.java
@@ -1,21 +1,22 @@
 package Strings;
 public class ReverseString{
-    public static void reverseString(char[] s){
-        int left = 0;
+    
+    public static void reverseString(char[]s){
+        int left =0;
         int right = s.length-1;
         while(left<right){
             char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-                left++;
-                right--;
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
         }
+        return;
     }
 
     public static void main(String[] args) {
-        // Input: s = ["h","e","l","l","o"]
-        // Output: ["o","l","l","e","h"]
-        char [] input = {'h','e','l','l','o'};
-        reverseString(input);
+        char[] str = {'h','e','l','l','o'};
+        reverseString(str);
     }
 }
+ 
