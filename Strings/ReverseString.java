a5f1491699045afcfd86519bebb8fe42570acab9
VikashTiwari23 <vikash.tiwari.236968@gmail.com>
2025-11-28 21:54:47 +0530

    Reverse String

---
+++ Strings/ReverseString.java
@@ -0,0 +1,20 @@
+public class ReverseString{
+    public static void reverseString(char[] s){
+        int left = 0;
+        int right = s.length-1;
+        while(left<right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+                left++;
+                right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Input: s = ["h","e","l","l","o"]
+        // Output: ["o","l","l","e","h"]
+        char [] input = {'h','e','l','l','o'};
+        reverseString(input);
+    }
+}
