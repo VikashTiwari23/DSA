c6dbd029729380b33a3bcc0f0936e13d4492bdf7
VikashTiwari23 <vikash.tiwari.236968@gmail.com>
2026-03-08 12:42:07 +0530

    Update MergeTwoSortedList.java

--- LinkedList/MergeTwoSortedList.java
+++ LinkedList/MergeTwoSortedList.java
@@ -27,3 +27,15 @@ public static ListNode mergeTwoLists(ListNode List1,ListNode List2){
         }
         return dummy.next;
 }
+public static ListNode mergeTwoLists(ListNode List1,ListNode List2){
+        if(List1==null) return List2;
+        if(List2==null) return List1;
+        if(List1.val<List2.val){
+            List1.next = mergeTwoLists(List1.next,List2);
+            return List1;
+        }
+        else {
+            List2.next = mergeTwoLists(List1,List2.next);
+            return List2; 
+        }
+}
